73aeff572a52672923c75d2f89e365b95d36fb90
sdf <dianfengsun@126.com>
2021-04-26 18:23:46 +0800

    添加人脸检测lib和include

--- app/src/main/cpp/native-lib.cpp
+++ app/src/main/cpp/native-lib.cpp
@@ -6,6 +6,6 @@ extern "C"
 JNIEXPORT jstring JNICALL
 Java_com_sdf_seetademo_jni_SeetaHandle_stringFromJNI(JNIEnv *env, jobject thiz) {
     char info[10000] = {0};
-    sprintf(info, "%d.%d.%d", cv::getVersionRevision(), cv::getVersionMinor(), cv::getVersionMajor());
+    sprintf(info, "%d.%d.%d", cv::getVersionMajor(),cv::getVersionMinor(),cv::getVersionRevision());
     return env->NewStringUTF(info);
 }
